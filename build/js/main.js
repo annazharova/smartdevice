89c78e6d75f295b0a1c5be6157710955cc6e23c8
Zharova Anna <zharova@restream.rt.ru>
2022-07-29 20:15:10 +0200

    Модальное окно

--- build/js/main.js
+++ build/js/main.js
@@ -97,12 +97,6 @@
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _utils_ios_vh_fix__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils/ios-vh-fix */ "./js/utils/ios-vh-fix.js");
 /* harmony import */ var _modules_modals_init_modals__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/modals/init-modals */ "./js/modules/modals/init-modals.js");
-function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
-
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
 
  // ---------------------------------
 
@@ -135,64 +129,6 @@ window.addEventListener('DOMContentLoaded', function () {
 // breakpoint.addListener(breakpointChecker);
 // breakpointChecker();
 // используйте .closest(el)
-
-var navMain = document.querySelector('.main-navigation');
-var navToggle = document.querySelector('.main-navigation__toggle');
-var body = document.querySelector('.page__body');
-var overlay = document.querySelector('.overlay');
-navMain.classList.remove('main-navigation--nojs');
-
-var closeMenu = function closeMenu() {
-  navMain.classList.remove('main-navigation--is-open');
-  navMain.classList.add('main-navigation--is-close');
-  body.style.overflowY = 'unset';
-  overlay.style.display = "none";
-};
-
-var openMenu = function openMenu() {
-  navMain.classList.add('main-navigation--is-open');
-  navMain.classList.remove('main-navigation--is-close');
-  body.style.overflowY = 'hidden';
-  overlay.style.display = "block";
-};
-
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-navigation--is-open')) {
-    closeMenu();
-  } else {
-    openMenu();
-  }
-});
-overlay.addEventListener('click', function () {
-  closeMenu();
-});
-var anchors = document.querySelectorAll('a[href*="#"]');
-
-var _iterator = _createForOfIteratorHelper(anchors),
-    _step;
-
-try {
-  var _loop = function _loop() {
-    var anchor = _step.value;
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      var blockID = anchor.getAttribute('href').substr(1);
-      document.getElementById(blockID).scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-      });
-      closeMenu();
-    });
-  };
-
-  for (_iterator.s(); !(_step = _iterator.n()).done;) {
-    _loop();
-  }
-} catch (err) {
-  _iterator.e(err);
-} finally {
-  _iterator.f();
-}
 
 /***/ }),
 
